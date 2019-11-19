25a2f3da26176058ebe5b1070b689e2934066cb8
jiayu <jiayuji@elpuig.xeill.net>
2019-11-20 00:15:09 +0100

    q

--- app/src/main/java/com/example/projrcte/HomeFragment.java
+++ app/src/main/java/com/example/projrcte/HomeFragment.java
@@ -13,10 +13,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Bottom1Fragment extends Fragment {
+public class HomeFragment extends Fragment {
 
 
-    public Bottom1Fragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +25,7 @@ public class Bottom1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom1, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
